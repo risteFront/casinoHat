33b63040886647ac503973145d6d63fcea848497
risteFront <risteee.123@hotmail.com>
2020-08-13 21:13:18 +0200

    full app demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navigation from "./component/navMenu";
+import axios from "axios";
+import GameCard from "./component/gameCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class CasinoHat extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Navigation></Navigation>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
